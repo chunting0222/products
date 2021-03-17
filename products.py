f5da4d03e47f76705799505b04a38fdfc779ae24
Kevin Tan <kevinsp20035708@gmail.com>
2021-03-18 00:21:09 +0800

    write as csv file

--- products.py
+++ products.py
@@ -10,3 +10,7 @@ print(products)
 
 for n in products:
 	print(n[0], '的價格是', n[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') # 在csv檔中，','是重要的分隔符號
