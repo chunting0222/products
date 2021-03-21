95f7966732442641e8685515a095885719581fee
Kevin Tan <kevinsp20035708@gmail.com>
2021-03-21 22:39:59 +0800

    Add encoding utf-8

--- products.py
+++ products.py
@@ -5,12 +5,14 @@ while True:
 		break
 	price = input('請輸入商品價格：')
 	p = [name, price]
+	price = int(price)
 	products.append(p) # p = [name, price]也可以移到此行
 print(products)
 
 for n in products:
 	print(n[0], '的價格是', n[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n') # 在csv檔中，','是重要的分隔符號
+		f.write(p[0] + ',' + str(p[1]) + '\n') # 在csv檔中，','是重要的分隔符號 #字串只能＋字串
